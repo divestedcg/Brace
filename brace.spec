7ee4f5d811e57344fcbd539e38e841c786d42ec2
Tad <tad@spotco.us>
2020-09-03 09:26:16 -0400

    Update userjs-brace.js

--- brace.spec
+++ brace.spec
@@ -1,5 +1,5 @@
 Name: brace
-Version: 20200826
+Version: 20200903
 Release: 1
 Summary: Increases privacy/security through various configs.
 License: GPLv3+
--- userjs-brace.js
+++ userjs-brace.js
@@ -1,32 +1,32 @@
 //Look
+pref("browser.ctrlTab.recentlyUsedOrder", false);
 pref("browser.tabs.drawInTitlebar", true);
+pref("general.smoothScroll", false);
 pref("widget.allow-client-side-decoration", true);
 pref("mailnews.start_page.enabled", false);
-pref("browser.ctrlTab.recentlyUsedOrder", false);
-pref("general.smoothScroll", false);
 
 //Privacy
-pref("general.useragent.updates.enabled", false);
-pref("browser.snippets.updateUrl", "");
-pref("browser.snippets.enabled", false);
-pref("browser.snippets.syncPromo.enabled", false);
-pref("browser.snippets.firstrunHomepage.enabled", false);
-pref("plugin.expose_full_path", false);
 pref("browser.link.open_newwindow_restriction", true);
+pref("browser.snippets.enabled", false);
+pref("browser.snippets.firstrunHomepage.enabled", false);
+pref("browser.snippets.syncPromo.enabled", false);
+pref("browser.snippets.updateUrl", "");
+pref("general.useragent.updates.enabled", false);
 pref("network.negotiate-auth.trusted-uris", "");
 pref("network.security.esni.enabled", true);
 pref("network.trr.custom_uri", "https://security.cloudflare-dns.com/dns-query");
-pref("messenger.status.reportIdle", false);
+pref("plugin.expose_full_path", false);
 pref("extensions.enigmail.autoWkdLookup", 0);
+pref("messenger.status.reportIdle", false);
 
 //Tracking Protection
 pref("browser.contentblocking.ui.enabled", true);
 pref("privacy.trackingprotection.enabled", true);
-pref("privacy.trackingprotection.fingerprinting.enabled", true);
 pref("privacy.trackingprotection.cryptomining.enabled", true);
+pref("privacy.trackingprotection.fingerprinting.enabled", true);
 
 //Security
 //pref("dom.security.https_only_mode", true);
 pref("security.webauth.u2f", true);
-pref("mailnews.message_display.disable_remote_image", true);
 pref("mail.phishing.detection.enabled", true);
+pref("mailnews.message_display.disable_remote_image", true);
