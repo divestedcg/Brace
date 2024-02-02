c5836917493df52af24b005fdd5df1cad5d1b6cf
Tavi <tavi@divested.dev>
2024-02-01 22:17:59 -0500

    Fixup
    Signed-off-by: Tavi <tavi@divested.dev>

--- brace.spec
+++ brace.spec
@@ -1,6 +1,6 @@
 Name: brace
 Version: 20240201
-Release: 1
+Release: 2
 Summary: Increases privacy/security through various configs.
 License: GPLv3+
 BuildArch: noarch
--- brace/etc/profile.d/brace-env-overrides.sh
+++ brace/etc/profile.d/brace-env-overrides.sh
@@ -27,7 +27,7 @@ export RADV_DEBUG=zerovram;
 export GST_VAAPI_ALL_DRIVERS=1;
 
 # disable thread local malloc cache
-GLIBC_TUNABLES='glibc.malloc.tcache_count=0'
+export GLIBC_TUNABLES='glibc.malloc.tcache_count=0'
 
 # set restrictive umask
 if [ "$(/usr/bin/id -ru)" -ge 1000 ] && [ "$(/usr/bin/id -u)" -ge 1000 ] && [ "$(/usr/bin/id -gn)" = "$(/usr/bin/id -un)" ]; then
