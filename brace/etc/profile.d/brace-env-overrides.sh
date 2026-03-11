b479380f3bd7d90a44953d1b807a59dd0e7b1234
Tavi <tavi@divested.dev>
2026-03-10 23:05:47 -0400

    More zerovram enablement
    Still no flag for Intel yet
    
    Also not entirely sure if those non-debug are limited to driconf or not
    
    Signed-off-by: Tavi <tavi@divested.dev>

--- brace/etc/profile.d/brace-env-overrides.sh
+++ brace/etc/profile.d/brace-env-overrides.sh
@@ -19,9 +19,13 @@ export CRYFS_NO_UPDATE_CHECK=true;
 
 # zero video RAM to prevent leakage
 # see (CC BY-SA 4.0): https://www.adlerweb.info/blog/2012/06/20/nvidia-x-org-video-ram-information-leak
-export R600_DEBUG=zerovram;
-export AMD_DEBUG=zerovram;
-export RADV_DEBUG=zerovram;
+export R600_DEBUG=zerovram; #r600
+export AMD_DEBUG=zerovram; #radeonsi
+export radeonsi_zerovram=true;
+export RADV_DEBUG=zerovram; #radv
+export radv_zero_vram=true;
+export NVK_DEBUG=zero_memory; #nvk
+export vk_zero_vram=true;
 
 # enable gstreamer va-api plugin on unsupported drivers
 export GST_VAAPI_ALL_DRIVERS=1;
