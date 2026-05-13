efb1bdd1371bdd0779df1af2a73c037699a64275
Tavi <tavi@divested.dev>
2026-05-13 14:28:17 -0400

    mr: resolve descriptions and add current list from dumps
    no functional change
    
    TODO: split some of these out into separate files like:
    -virt
    -platform
    -devices
    
    Signed-off-by: Tavi <tavi@divested.dev>

--- module_restricter/merge.sh
+++ module_restricter/merge.sh
@@ -1,29 +1,26 @@
-sort -u kernels/*.txt > index.txt
-
-grep -v "^#" allowed-static.txt > allowed-static-wc.txt;
-sort -u allowed-static-wc.txt systems/*.txt > allowed.txt;
-rm allowed-static-wc.txt;
+cat kernels/*.txt | grep -v "^#" | sed 's/ #.*$//' | sort -u > index.tmp.txt;
+cat allowed-static*.txt systems/*.txt | grep -v "^#" | sed 's/ #.*$//' | sort -u > allowed.tmp.txt;
 
 rm result.conf;
 
 while IFS= read -r line
 do
 	echo "blacklist $line" >> result.conf;
-done < index.txt;
+done < index.tmp.txt;
 
 while IFS= read -r line
 do
 	sed -i "/ $line$/ s/^/# /" result.conf;
-done < allowed.txt;
+done < allowed.tmp.txt;
 
 echo "Total modules:"
 wc -l result.conf
 echo "Expected allowed modules:"
-wc -l allowed.txt;
+wc -l allowed.tmp.txt;
 echo "Actual allowed modules:"
 grep -c "^#" result.conf;
 echo "Blocked modules:"
 grep -c -v "^#" result.conf;
 
-rm allowed.txt;
-rm index.txt
+rm allowed.tmp.txt;
+rm index.tmp.txt;
--- module_restricter/resolve_kernel.sh
+++ module_restricter/resolve_kernel.sh
@@ -1,4 +1,6 @@
 for kmod in $(find /lib/modules/$1/ -name *.ko.xz); do
-	modinfo "$kmod" | grep "^name: " | sed "s/.* //" >> kernels/$1.txt;
+	name=$(modinfo "$kmod" | grep -m1 "^name: " | sed "s/.* //");
+	description=$(modinfo "$kmod" | grep -m1 "^description: " | sed "s/.*:[[:blank:]]*//");
+	echo "$name #$description" >> kernels/$1.txt;
 done
 sort -u -o kernels/$1.txt kernels/$1.txt;
